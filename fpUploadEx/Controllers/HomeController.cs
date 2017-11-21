246d8f965c8a1630025710176a02682d89491a73
yogpande <yogpande@users.noreply.github.com>
2017-11-21 08:53:39 +0530

    fdgfdg
    ghdfgfdfdgdf

---
+++ fpUploadEx/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fpUploadEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Index(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                string path = "~/images/" + file.FileName;
+                file.SaveAs(Server.MapPath(path));
+                return "OK";
+            }
+            else
+            return "try again";
+        }
+    }
+}
